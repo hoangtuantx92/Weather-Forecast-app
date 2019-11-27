3d385328e876906d7514814cbe75c79c77f28d3b
Kevin Do <hoangtuantx92@yahoo.com>
2019-11-27 14:51:43 -0600

    kevin func

---
+++ Controllers/WeatherSearchController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace API_Workshop.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherSearchController : ControllerBase
+    {
+         private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        public IEnumerable<WeatherForecast> weather()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+     [HttpGet]
+     [Route("{negativedegree}")]
+     public IEnumerable<WeatherForecast> GetTemperature() {
+         
+        return weather().Where(t => t.TemperatureC < 0).ToArray();
+     }
+    }
+}
