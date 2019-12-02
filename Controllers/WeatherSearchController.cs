ad918b59e982ed96c0c38d5a28d39e5d933ae788
Kevin Do <hoangtuantx92@yahoo.com>
2019-12-01 20:27:58 -0600

    weather app

--- Controllers/WeatherSearchController.cs
+++ Controllers/WeatherSearchController.cs
@@ -14,11 +14,18 @@ namespace API_Workshop.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+
+        private static readonly string[] Cities = new[]
+        {
+            "Houston", "Los Angeles", "New York", "San Francisco", "Dallas", "Des Moines"
+        };
+
         public IEnumerable<WeatherForecast> weather()
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                City = Cities[rng.Next(Cities.Length)],
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -6,6 +6,8 @@ namespace API_Workshop
     {
         public DateTime Date { get; set; }
 
+         public string City { get; set; }
+
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
